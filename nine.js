2c403a6149b919c6f74111fed4d7920bf120400d
Sami ullah <samiullahyounus10@gmail.com>
2025-02-03 11:34:00 -0800

    Add files via upload

---
+++ nine.js
@@ -0,0 +1,4 @@
+var massege=("I am trying to learnfrom a Book A Smarter\nway learn javaScript")
+alert(massege)
+const contect='efwehafkjhrufer';
+document.getElementById('content').innerHTML = content;
